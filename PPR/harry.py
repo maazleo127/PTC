91848fdc80dccdfe0d02aa2fe625c1725e2c664f
maazleo127 <125278759+maazleo127@users.noreply.github.com>
2023-05-02 22:36:15 +0530

    new

---
+++ PPR/harry.py
@@ -0,0 +1,26 @@
+# a=float(input("Enter your first number:"))
+# b=float(input("Enter your second number:"))
+# op=input("Enter the operation you want to perform (+,-,*,/):")
+# if (a,b)==(45,3) and op=="*":
+#     print("The answer is:555")
+# elif (a,b)==(56,9) and op=="+":
+#     print("The answer is:77")
+# elif (a,b)==(56,6) and op=="/":
+#     print("The answer is:4")
+# else:
+#     if op=="+":
+#         print("The answer is:",a+b)
+#     elif op=="-":
+#         print("The answer is:",a-b)
+#     elif op=="*":
+#         print("The answer is:",a*b)
+#     elif op == "/":
+#         print("The answer is:",a/b)
+#     else:
+#         print("Enter a valid operation.")
+# pow = [x * 2 for x in range(1,11)]
+# print(pow)
+print([ i.lower() for i in "HELLO"])
+
+
+
