82ea2546bee3dbf37fe4198c24bc24d1ea2718ea
maazleo127 <125278759+maazleo127@users.noreply.github.com>
2023-05-06 21:51:32 +0530

    new

--- PPR/function.py
+++ PPR/function.py
@@ -18,9 +18,30 @@
 # # a(2,2,2)
 # cube= lambda x:(x**3)
 # print(cube(3))
-nums=[1,2,3,4,5]
-def square(n):
-    return (n*n)
+# nums=[1,2,3,4,5]
+# def square(n):
+#     return (n*n)
 
-result=list(map(square,nums))
-print((result))
+# result=list(map(square,nums))
+# print((result))
+# def even(n):
+#     if n%2==0:
+#         return True
+#     else:
+#         return False
+
+# result=filter(even,[11,22,33,44,55,66,77,88])
+# print(list(result))
+
+class Human:   #class
+    name=""    #attribute
+    def __init__(self,name1): #constructor
+        self.name=name1     
+
+h1=Human("raj") #object containing attributes value
+h2=Human('dev')  #
+
+print(h1.name,h2.name)
+
+        
+
